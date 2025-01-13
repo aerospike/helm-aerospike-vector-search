f6efba44173b8743287055c6f0ea454c906fbd55
David Gerchikov <dgerchikov@aerospike.com>
2025-01-13 18:13:30 +0200

    Commit

--- avs-init-container/cmd/main.go
+++ avs-init-container/cmd/main.go
@@ -2,54 +2,147 @@ package main
 
 import (
 	"aerospike.com/avs-init-container/v2/util"
+	"context"
 	"encoding/json"
 	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"os"
-	"regexp"
+	"sigs.k8s.io/yaml"
 )
 
-const NODE_ROLES_KEY string = "node-roles"
+const (
+	AVS_NODE_LABEL_KEY   = "avs-node-label"
+	NODE_ROLES_KEY       = "node-roles"
+	AVS_CONFIG_FILE_PATH = "/etc/aerospike-vector-search/aerospike-vector-search.yml"
+)
 
-func getAerospikeVectorSearchRoles() (map[string], error) {
+func getNodeLabels() (string, error) {
 
-	aerospikeVectorSearchRolesEnvVariable := os.Getenv("AEROSPIKE_VECTOR_SERACH_ROLES")
-	if aerospikeVectorSearchRolesEnvVariable == "" {
-		return nil, nil
+	nodeName := os.Getenv("NODE_NAME")
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return "", err
 	}
 
-	var aerospikeVectorSearchRoles map[string]interface{}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return "", err
+	}
 
-	err := json.Unmarshal([]byte(aerospikeVectorSearchRolesEnvVariable), &aerospikeVectorSearchRoles)
+	node, err := clientset.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	if label, ok := node.Labels[AVS_NODE_LABEL_KEY]; ok {
+		return label, nil
+	}
+	return "", nil
+}
+
+func getAerospikeVectorSearchRoles() (map[string]interface{}, error) {
+
+	label, err := getNodeLabels()
 	if err != nil {
 		return nil, err
 	}
 
-	pod_name := os.Getenv("POD_NAME")
-	if pod_name == "" {
-		return nil, fmt.Errorf("%s", "POD_NAME env variable is empty")
+	aerospikeVectorSearchRolesEnvVariable := os.Getenv("AEROSPIKE_VECTOR_SEARCH_NODE_ROLES")
+	if aerospikeVectorSearchRolesEnvVariable == "" {
+		return nil, nil
+	}
+	var aerospikeVectorSearchRoles map[string]interface{}
+
+	err = json.Unmarshal([]byte(aerospikeVectorSearchRolesEnvVariable), &aerospikeVectorSearchRoles)
+	if err != nil {
+		return nil, err
 	}
 
-	re := regexp.MustCompile(`\d+$`)
-	match := re.FindString(pod_name)
-	if match == "" {
-		return nil, fmt.Errorf("%s", "POD_NAME env variable has bad format")
-	}
-
-	if role, ok := aerospikeVectorSearchRoles[fmt.Sprintf("node-%s", match)]; ok {
+	if role, ok := aerospikeVectorSearchRoles[label]; ok {
 		retval := make(map[string]interface{})
 		retval[NODE_ROLES_KEY] = role
 		return retval, nil
 	}
 
 	return nil, nil
+}
 
+func setRoles(aerospikeVectorSearchConfig map[string]interface{}) error {
+
+	roles, err := getAerospikeVectorSearchRoles()
+	if err != nil {
+		return err
+	}
+
+	if cluster, ok := aerospikeVectorSearchConfig["cluster"].(map[string]interface{}); ok {
+		cluster[NODE_ROLES_KEY] = roles[NODE_ROLES_KEY]
+		aerospikeVectorSearchConfig["cluster"] = cluster
+	} else {
+		return fmt.Errorf("Was not able to get cluster")
+	}
+
+	return nil
+}
+
+func writeConfig(aerospikeVectorSearchConfig map[string]interface{}) error {
+	config, err := yaml.Marshal(aerospikeVectorSearchConfig)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(AVS_CONFIG_FILE_PATH)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	_, err = file.Write(config)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func run() int {
+
+	aerospikeVectorSearchConfigEnvVariable := os.Getenv("AEROSPIKE_VECTOR_SEARCH_CONFIG")
+	if aerospikeVectorSearchConfigEnvVariable == "" {
+		fmt.Println("Aerospike Vector Search configuration is empty")
+		return 1
+	}
+
+	var aerospikeVectorSearchConfig map[string]interface{}
+
+	err := json.Unmarshal([]byte(aerospikeVectorSearchConfigEnvVariable), &aerospikeVectorSearchConfig)
+	if err != nil {
+		fmt.Println(err)
+		return util.ToExitVal(err)
+	}
+
+	err = setRoles(aerospikeVectorSearchConfig)
+	if err != nil {
+		fmt.Println(err)
+		return util.ToExitVal(err)
+	}
+
+	err = writeConfig(aerospikeVectorSearchConfig)
+	if err != nil {
+		fmt.Println(err)
+		return util.ToExitVal(err)
+	}
 
 	return util.ToExitVal(nil)
 }
 
 func main() {
+	fmt.Println("Starting")
 	os.Exit(run())
 }
