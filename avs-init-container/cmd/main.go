45e462078749d2f9de56d3cac158830146ce4688
David Gerchikov <dgerchikov@aerospike.com>
2025-02-03 15:03:00 +0200

    commit

--- avs-init-container/cmd/main.go
+++ avs-init-container/cmd/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"aerospike.com/avs-init-container/v2/util"
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
+	"sync"
+
+	"aerospike.com/avs-init-container/v2/util"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"os"
 	"sigs.k8s.io/yaml"
 )
 
@@ -18,21 +21,176 @@ const (
 	AVS_CONFIG_FILE_PATH = "/etc/aerospike-vector-search/aerospike-vector-search.yml"
 )
 
+type NodeInfoSingleton struct {
+	node    *v1.Node
+	service *v1.Service
+	err     error
+}
+
+var (
+	instance *NodeInfoSingleton
+	once     sync.Once
+)
+
+func GetNodeInstance() (*v1.Node, *v1.Service, error) {
+	once.Do(func() {
+		nodeName := os.Getenv("NODE_NAME")
+		if nodeName == "" {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     fmt.Errorf("NODE_NAME environment variable is not set"),
+			}
+			return
+		}
+
+		serviceName := os.Getenv("SERVICE_NAME")
+		if nodeName == "" {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     fmt.Errorf("POD_NAME environment variable is not set"),
+			}
+			return
+		}
+
+		podNamespace := os.Getenv("POD_NAMESPACE")
+		if nodeName == "" {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     fmt.Errorf("POD_NAMESPACE environment variable is not set"),
+			}
+			return
+		}
+
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     err,
+			}
+			return
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     err,
+			}
+			return
+		}
+
+		node, err := clientset.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
+		if err != nil {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     err,
+			}
+			return
+		}
+
+		service, err := clientset.CoreV1().Services(podNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
+		if err != nil {
+			instance = &NodeInfoSingleton{
+				node:    nil,
+				service: nil,
+				err:     err,
+			}
+			return
+		}
+
+		instance = &NodeInfoSingleton{
+			node:    node,
+			service: service,
+			err:     err,
+		}
+	})
+
+	return instance.node, instance.service, instance.err
+}
+
+func getNodeIp() (string, int32, error) {
+
+	externalIP, internalIP := "", ""
+
+	node, service, err := GetNodeInstance()
+	if err != nil {
+		return "", 0, err
+	}
+
+	var nodePort int32 = 0
+	for _, port := range service.Spec.Ports {
+		if port.NodePort != 0 {
+			nodePort = port.NodePort
+			break
+		}
+	}
+	if nodePort == 0 {
+		return "", 0, fmt.Errorf("node port is not set")
+	}
+
+	for _, addr := range node.Status.Addresses {
+		if addr.Type == v1.NodeInternalIP {
+			internalIP = addr.Address
+		} else if addr.Type == v1.NodeExternalIP {
+			externalIP = addr.Address
+		}
+	}
+
+	if externalIP != "" {
+		return externalIP, nodePort, nil
+	}
+
+	if internalIP != "" {
+		return internalIP, nodePort, nil
+	}
+
+	return "", 0, nil
+}
+
+func setAdvertisedListeners(aerospikeVectorSearchConfig map[string]interface{}) error {
+
+	nodeIP, nodePort, err := getNodeIp()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Node IP: %s Port: %d\n", nodeIP, nodePort)
+	service, ok := aerospikeVectorSearchConfig["service"].(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("Was not able to get service")
+	}
+
+	ports, ok := service["ports"].(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("Was not able to get service.ports")
+	}
+
+	for _, portConfig := range ports {
+		portMap, ok := portConfig.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		portMap["advertised-listeners"] = map[string][]map[string]interface{}{
+			"default": {
+				{
+					"address": nodeIP,
+					"port":    nodePort,
+				},
+			},
+		}
+	}
+	return nil
+}
+
 func getNodeLabels() (string, error) {
 
-	nodeName := os.Getenv("NODE_NAME")
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return "", err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return "", err
-	}
-
-	node, err := clientset.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
+	node, _, err := GetNodeInstance()
 	if err != nil {
 		return "", err
 	}
@@ -136,6 +294,12 @@ func run() int {
 	}
 
 	err = setRoles(aerospikeVectorSearchConfig)
+	if err != nil {
+		fmt.Println(err)
+		return util.ToExitVal(err)
+	}
+
+	err = setAdvertisedListeners(aerospikeVectorSearchConfig)
 	if err != nil {
 		fmt.Println(err)
 		return util.ToExitVal(err)
