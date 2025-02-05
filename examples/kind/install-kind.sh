a59ee99ac7dc17f84c36091bd6b9ed1bb44661cc
Joe M. <jmartin@aerospike.com>
2025-02-04 19:10:23 -0800

    VEC-519 Allow configuring advertised listeners from within helm chart  (#20)
    feat: allow configuring advertised listeners ( @davi17g )
    ---------
    
    Co-authored-by: David Gerchikov <dgerchikov@aerospike.com>

--- examples/kind/install-kind.sh
+++ examples/kind/install-kind.sh
@@ -10,13 +10,13 @@ for item in "${REQUISITES[@]}"; do
   fi
 done
 
-if [ ! -f "$WORKSPACE/aerospike-vector-search/kind/config/features.conf" ]; then
+if [ ! -f "$WORKSPACE/examples/kind/config/features.conf" ]; then
   echo "features.conf Not found"
   exit 1
 fi
 
 echo "Installing Kind"
-kind create cluster --config "$WORKSPACE/aerospike-vector-search/kind/config/kind-cluster.yaml"
+kind create cluster --config "$WORKSPACE/examples/kind/config/kind-cluster.yaml"
 kubectl cluster-info --context kind-kind
 
 echo "Deploying AKO"
@@ -40,13 +40,13 @@ kubectl create clusterrolebinding aerospike-cluster \
 
 echo "Set Secrets for Aerospike Cluster"
 kubectl --namespace aerospike create secret generic aerospike-secret \
---from-file=features.conf="$WORKSPACE/aerospike-vector-search/kind/config/features.conf"
+--from-file=features.conf="$WORKSPACE/examples/kind/config/features.conf"
 kubectl --namespace aerospike create secret generic auth-secret --from-literal=password='admin123'
 
 
 sleep 5
 echo "Deploy Aerospike Cluster"
-kubectl apply -f "$WORKSPACE/aerospike-vector-search/examples/kind/aerospike.yaml"
+kubectl apply -f "$WORKSPACE/examples/examples/kind/aerospike.yaml"
 
 sleep 5
 echo "Waiting for Aerospike Cluster"
@@ -57,49 +57,32 @@ while true; do
     break
   fi
 done
-
-
-echo "Deploy MetalLB"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.4/config/manifests/metallb-native.yaml
-kubectl wait --namespace metallb-system \
-                --for=condition=ready pod \
-                --selector=app=metallb \
-                --timeout=90s
-kubectl apply -f "$WORKSPACE/aerospike-vector-search/kind/config/metallb-config.yaml"
-
-
-echo "Deploying Istio"
-helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm repo update
-helm install istio-base istio/base --namespace istio-system --set defaultRevision=default --create-namespace --wait
-helm install istiod istio/istiod --namespace istio-system --create-namespace --wait
-helm install istio-ingress istio/gateway \
---values "$WORKSPACE/aerospike-vector-search/kind/config/istio-ingressgateway-values.yaml" \
---namespace istio-ingress \
---create-namespace \
---wait
-
-kubectl apply -f "$WORKSPACE/aerospike-vector-search/kind/config/gateway.yaml"
-kubectl apply -f "$WORKSPACE/aerospike-vector-search/kind/config/virtual-service-vector-search.yaml"
+#
+#
+#echo "Deploy MetalLB"
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.4/config/manifests/metallb-native.yaml
+#kubectl wait --namespace metallb-system \
+#                --for=condition=ready pod \
+#                --selector=app=metallb \
+#                --timeout=90s
+#kubectl apply -f "$WORKSPACE/examples/kind/config/metallb-config.yaml"
+#
+#
+#echo "Deploying Istio"
+#helm repo add istio https://istio-release.storage.googleapis.com/charts
+#helm repo update
+#helm install istio-base istio/base --namespace istio-system --set defaultRevision=default --create-namespace --wait
+#helm install istiod istio/istiod --namespace istio-system --create-namespace --wait
+#helm install istio-ingress istio/gateway \
+#--values "$WORKSPACE/examples/kind/config/istio-ingressgateway-values.yaml" \
+#--namespace istio-ingress \
+#--create-namespace \
+#--wait
+#
+#kubectl apply -f "$WORKSPACE/examples/kind/config/gateway.yaml"
+#kubectl apply -f "$WORKSPACE/examples/kind/config/virtual-service-vector-search.yaml"
 
 sleep 30
 echo "Deploy AVS"
-helm install avs "$WORKSPACE/aerospike-vector-search" \
---values "$WORKSPACE/aerospike-vector-search/examples/kind/avs-kind-values.yaml" --namespace aerospike
-
-echo "Deploying Quote-Search"
-
-git clone \
---depth 1 \
---branch main \
---no-checkout https://github.com/aerospike/aerospike-vector-search-examples.git
-cd aerospike-vector-search-examples
-git sparse-checkout set kubernetes/helm/quote-semantic-search
-git checkout main
-cd -
-
-helm install quote-search "$PWD/aerospike-vector-search-examples/kubernetes/helm/quote-semantic-search" \
---values "$WORKSPACE/aerospike-vector-search/kind/config/quote-semantic-search-values.yaml" \
---namespace aerospike \
---wait \
---timeout 7m0s
+helm install avs-app "$WORKSPACE/charts/aerospike-vector-search" \
+--values "$WORKSPACE/avs-init-container/avs-kind-values.yaml" --namespace aerospike
